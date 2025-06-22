06a1cca211531d0b5fe2ea6065f9e15eee382f9d
luciano <lucho.2012.tandil@gmail.com>
2025-06-22 14:31:43 -0300

    [#273] Se agrega posibilidad de parametrizar opciones preconfigurables con su numero de telefono

--- development/my-vue-app/src/App.vue
+++ development/my-vue-app/src/App.vue
@@ -1,9 +1,21 @@
 <script setup>
 import WhatsAppWidget from './components/WhatsAppWidget.vue'
+import { ref } from 'vue'
+
+const quick_replies = ref([
+  {
+    text: "Quick reply 1",
+    phone: "542494000000"
+  },
+  {
+    text: "Quick reply 2",
+    phone: "542494000001"
+  },
+])
 </script>
 
 <template>
-  <WhatsAppWidget phone="542494000000" />
+  <WhatsAppWidget phone="542494000000" :quick_replies="quick_replies" />
   <a href="https://github.com/Greenborn/vue3-whatsapp-widget"> GitHub</a>
   <button>Botón</button>
 </template>
